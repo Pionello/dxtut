2cb83a5db6416d7a3c89e152fd4c8e50f08c6042
Yaroslav Pióntko <yaroslav.rsr@gmail.com>
2022-07-24 15:34:20 +0300

    Added body to WinMain func

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,11 @@
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
-	int nCmdShow);
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	MessageBox(NULL, L"Alert!", L"Window header", MB_ICONEXCLAMATION | MB_OK);
+	return 0;
+}
 
